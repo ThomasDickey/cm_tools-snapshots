c17e0f097ff5ba4abe94e7e4c532570ddf2e74d2
Thomas E. Dickey <dickey@invisible-island.net>
1990-01-24 13:15:02 -0500

    snapshot of project "cm_tools", label t19900124

--- src/baseline/src/baseline.c
+++ src/baseline/src/baseline.c
@@ -1,5 +1,5 @@
 #ifndef	lint
-static	char	Id[] = "$Id: baseline.c,v 5.4 1989/11/07 08:19:19 dickey Exp $";
+static	char	Id[] = "$Id: baseline.c,v 6.0 1990/01/03 12:53:06 ste_cm Rel $";
 #endif	lint
 
 /*
@@ -7,9 +7,23 @@ static	char	Id[] = "$Id: baseline.c,v 5.4 1989/11/07 08:19:19 dickey Exp $";
  * Author:	T.E.Dickey
  * Created:	24 Oct 1989
  * $Log: baseline.c,v $
- * Revision 5.4  1989/11/07 08:19:19  dickey
- * corrected code which infers current-baseline
+ * Revision 6.0  1990/01/03 12:53:06  ste_cm
+ * BASELINE Thu Mar 29 07:37:55 1990 -- maintenance release (SYNTHESIS)
  *
+ *		Revision 5.7  90/01/03  12:53:06  dickey
+ *		corrected code which infers baseline revision (if no rcs
+ *		directory existed, this did not notice!)
+ *		
+ *		Revision 5.6  90/01/03  11:58:27  dickey
+ *		added logic (for "-l" option) so that we can allow a locked
+ *		file to be baselined (if it is locked, but not changed).
+ *		
+ *		Revision 5.5  90/01/03  11:31:29  dickey
+ *		added "-l" option for stluka
+ *		
+ *		Revision 5.4  89/11/07  08:19:19  dickey
+ *		corrected code which infers current-baseline
+ *		
  *		Revision 5.3  89/11/07  08:08:50  dickey
  *		corrected treatment of "-r" option (recur only when asked)
  *		
@@ -60,6 +74,7 @@ static	char	r_option[80];
 				/* options */
 static	int	a_opt;		/* all-directory scan */
 static	int	fast_opt;
+static	int	lock_up;	/* leave files locked */
 static	char	*m_text;	/* message-text */
 static	int	no_op;		/* no-op mode */
 static	int	recur;
@@ -114,18 +129,21 @@ char	*path;
 }
 
 static
-baseline(path, name)
+baseline(path, name, edited)
 char	*path;
 char	*name;
+time_t	edited;
 {
 	auto	char	args[BUFSIZ];
 	auto	char	*version,
 			*locker;
 	auto	time_t	date;
 	auto	int	cmp;
+	auto	int	pending;	/* true if lock implies change */
 
 	purge_rights(path);
 	rcslast (path, name, &version, &date, &locker);
+	pending = (!lock_up) || (date != edited);
 
 	if (*version == '?') {
 		if (istextfile(name)) {
@@ -143,21 +161,24 @@ char	*name;
 	} else if (cmp == 0) {
 		PRINTF("** %s (already baselined)\n", name);
 		return;
-	} else if (*locker != '?') {
+	} else if (*locker != '?' && pending) {
 		WARN "?? %s (locked by %s)\n", name, locker);
 		exit(FAIL);
 		/*NOTREACHED*/
 	}
 	PRINTF("** %s\n", name);
 
-	*args = EOS;
-	catarg(args, "-l");
-	quiet_opt(args);
-	catarg(args, name);
-	doit("checkout", args, !no_op);
+	if (*locker == '?') {		/* wasn't locked, must do so now */
+		*args = EOS;
+		catarg(args, "-l");
+		quiet_opt(args);
+		catarg(args, name);
+		doit("checkout", args, !no_op);
+	}
 
 	FORMAT(args, "-r%d.0 -f -u ", revision);
 	quiet_opt(args);
+	if (lock_up)	catarg(args, "-l");
 	catarg(args, m_option);
 	catarg(args, "-sRel");
 	catarg(args, name);
@@ -190,7 +211,7 @@ struct	stat	*sp;
 	} else if (isFILE(sp->st_mode)) {
 		if (level > 0)
 			track_wd(path);
-		baseline(path,name);
+		baseline(path,name, sp->st_mtime);
 	} else
 		ok_acc = -1;
 
@@ -219,9 +240,10 @@ char	*name;
 		rcslast (".",
 			vcs_file(rcs_dir(), vname, FALSE),
 			&version, &date, &locker);
-		if (*version != '?'
-		&&  sscanf(version, "%d.", &revision) > 0)
-			infer_rev = TRUE;
+		if (*version == '?'
+		||  sscanf(version, "%d.", &revision) <= 0)
+			revision = 2;
+		infer_rev = TRUE;
 	}
 	FORMAT(r_option, "%d.0", revision);
 
@@ -234,12 +256,16 @@ char	*name;
 usage()
 {
 	static	char	*tbl[] = {
- "Usage: baseline [options] files"
+ "Usage: baseline [options] [files]"
+,""
+,"Bumps versions of rcs files to the specified integer value (i.e., 2.0)."
+,"If no files are specified, all files in the current directory are assumed."
 ,""
 ,"Options are:"
 ,"  -{integer}  baseline version (must be higher than last baseline)"
 ,"  -m{text}    append reason for baseline to date-message"
 ,"  -f          fast (don't run \"permit\" first)"
+,"  -l          leave files locked after baselining"
 ,"  -n          no-op (show what would be done, but don't do it)"
 ,"  -r          recur when lower-level directories are found"
 ,"  -v          verbose (shows details)"
@@ -279,6 +305,7 @@ char	*argv[];
 					switch (*s) {
 					case 'a':	a_opt++;	break;
 					case 'f':	fast_opt++;	break;
+					case 'l':	lock_up++;	break;
 					case 'n':	no_op++;	break;
 					case 'r':	recur = 999;	break;
 					case 'v':	verbose++;	break;
