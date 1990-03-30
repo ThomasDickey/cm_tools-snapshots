5812675941f73ddb9dc4a7dc43e329d7a08df37d
Thomas E. Dickey <dickey@invisible-island.net>
1990-03-30 12:50:15 -0500

    snapshot of project "cm_tools", label t19900330

--- src/checkup/src/checkup.c
+++ src/checkup/src/checkup.c
@@ -1,5 +1,5 @@
 #ifndef	lint
-static	char	Id[] = "$Id: checkup.c,v 5.2 1990/02/07 12:58:53 dickey Exp $";
+static	char	Id[] = "$Id: checkup.c,v 8.0 1990/03/30 12:50:15 ste_cm Rel $";
 #endif	lint
 
 /*
@@ -7,45 +7,73 @@ static	char	Id[] = "$Id: checkup.c,v 5.2 1990/02/07 12:58:53 dickey Exp $";
  * Author:	T.E.Dickey
  * Created:	31 Aug 1988
  * $Log: checkup.c,v $
- * Revision 5.2  1990/02/07 12:58:53  dickey
- * added "-p" option to generate relative pathnames
+ * Revision 8.0  1990/03/30 12:50:15  ste_cm
+ * BASELINE Mon Aug 13 15:06:41 1990 -- LINCNT, ADA_TRANS
+ *
+ *		Revision 7.0  90/03/30  12:50:15  ste_cm
+ *		BASELINE Mon Apr 30 09:54:01 1990 -- (CPROTO)
+ *		
+ *		Revision 6.1  90/03/30  12:50:15  dickey
+ *		corrected code which checks for suppressed extensions (if a
+ *		wildcard is given, we don't really know the exact length that
+ *		would be matched).
+ *		
+ *		Revision 6.0  90/03/28  11:21:39  ste_cm
+ *		BASELINE Thu Mar 29 07:37:55 1990 -- maintenance release (SYNTHESIS)
+ *		
+ *		Revision 5.5  90/03/28  11:21:39  dickey
+ *		display the version-number in "-d" listing where I was only
+ *		showing "ok".
+ *		
+ *		Revision 5.4  90/03/28  10:34:24  dickey
+ *		added "-c" (configuration-list) option so user can obtain a
+ *		list of the working files in a tree together with their
+ *		revision codes.
+ *		
+ *		Revision 5.3  90/03/28  08:08:05  dickey
+ *		added "-i" option.  made both "-i" and "-x" options permit
+ *		wildcards.  enhanced usage-message.
+ *		
+ *		Revision 5.2  90/02/07  12:58:53  dickey
+ *		added "-p" option to generate relative pathnames
  *
  *		Revision 5.1  90/02/07  11:14:26  dickey
  *		added "-l" option to reopen stderr so we can easily pipe the
  *		report to a file
- *		
+ *
  *		Revision 5.0  89/10/26  10:20:32  ste_cm
- *		BASELINE Fri Oct 27 12:27:25 1989 -- apollo SR10.1 mods + ADA_PITS 4.0
- *		
+ *		BASELINE Fri Oct 27 12:27:25 1989 -- apollo SR10.1 mods
+ *		+ ADA_PITS 4.0
+ *
  *		Revision 4.1  89/10/26  10:20:32  dickey
  *		use new procedure 'istextfile()'
- *		
+ *
  *		Revision 4.0  89/08/17  13:59:45  ste_cm
  *		BASELINE Thu Aug 24 09:31:25 EDT 1989 -- support:navi_011(rel2)
- *		
+ *
  *		Revision 3.3  89/08/17  13:59:45  dickey
  *		if "-r" and "-o" are both set, use "-r" to limit the set of
  *		files shown as obsolete, rather than to select working files
  *		for date-comparison.
- *		
+ *
  *		Revision 3.2  89/08/17  13:18:03  dickey
  *		expanded usage-message to multi-line display.  corrected a
  *		place where "?" constant was overwritten.  added "-d" and
  *		"-o" options.
- *		
+ *
  *		Revision 3.1  89/08/17  09:02:42  dickey
  *		modified "-r" option to support reverse comparison (if user
  *		suffixes a "+" to option-value).
- *		
+ *
  *		Revision 3.0  88/09/26  07:56:39  ste_cm
  *		BASELINE Mon Jun 19 13:17:34 EDT 1989
- *		
+ *
  *		Revision 2.0  88/09/26  07:56:39  ste_cm
  *		BASELINE Thu Apr  6 09:32:39 EDT 1989
- *		
+ *
  *		Revision 1.7  88/09/26  07:56:39  dickey
  *		sccs2rcs keywords
- *		
+ *
  *		26 Sep 1988, corrected use of 'sameleaf()' -- had wrong args!
  *		19 Sep 1988, added '-a' option.
  *		02 Sep 1988, use 'rcs_dir()', 'sccs_dir()'
@@ -61,38 +89,7 @@ static	char	Id[] = "$Id: checkup.c,v 5.2 1990/02/07 12:58:53 dickey Exp $";
  *		If standard output is piped (e.g., to a file), the list of
  *		offending filenames will be written there as well.
  *
- * Options:	-a	scans directory-names beginning with '.' (normally
- *			ignored).
- *		-d	debug, shows all names and dates
- *		-o	reports "obsolete" files, i.e., those archives for
- *			which there is no corresponding working-file.
- *		-p	sends pathnames in relative-format.  This is useful
- *			when a set of full pathnames would be too long for the
- *			prevailing shell.
- *		-r REV	reports all working files whose highest checked-in
- *			version is below REV.  (A "+" suffixed causes the
- *			report to reverse, showing files above REV).
- *		-q (-s)	suppress report, used when only a list of names is
- *			wanted.
- *		-t	sets up a default list of extensions to be ignored.
- *
- *		-x XXX	specifies an extension to be ignored.  The first
- *			character in the option-value is used as the delimiter.
- *			If repeated, it also marks off a second extension,
- *			which must correspond to an existing file before the
- *			file with the end-extension is ignored.  For example,
- *
- *				-x .e.c
- *
- *			causes all ".c" files which have a corresponding
- *			".e" file to be ignored.
- *
- *			If more than one "-x" option is given, the most recent
- *			is processed first.
- *
- *		-v	verbose option directs this program to print the names
- *			which cannot be processed (i.e., sockets, and stuff that
- *			cannot be 'stat'ed.
+ * Options:	(see 'usage()')
  */
 
 #define	DIR_PTYPES
@@ -130,6 +127,7 @@ typedef	struct	_exts	{
 
 static	EXTS	*exts;			/* "-x" and "-t" extension list */
 static	int	allnames;		/* "-a" option */
+static	int	config_rev;		/* "-c" option */
 static	int	debug;			/* "-d" option */
 static	int	obsolete;		/* "-o" option */
 static	int	verbose;		/* "-v" option */
@@ -140,6 +138,22 @@ static	int	reverse;		/* true if we reverse revision-test */
 static	char	*original;		/* original directory, for "-p" */
 
 /*
+ * Define a new leaf-name to ignore.  Piggyback on the "-x" data structure by
+ * defining the ".no_ext" member to null.
+ */
+static
+ignore(string)
+char	*string;
+{
+	EXTS	*savep = exts;
+	exts = ALLOC(EXTS,1);
+	exts->link = savep;
+	exts->no_ext = 0;
+	exts->if_ext = txtalloc(string);
+	if (debug) TELL "ignore leaf \"%s\"\n", string);
+}
+
+/*
  * Define a new extension-to-ignore
  */
 static
@@ -147,8 +161,9 @@ extension(string)
 char	*string;
 {
 	EXTS	*savep = exts;
-	char	*s = strrchr(string, *string);
+	char	*s = strrchr(string, (size_t)*string);
 	char	bfr[BUFSIZ];
+	int	show = (debug || verbose > 0);
 
 	exts = ALLOC(EXTS,1);
 	exts->link = savep;
@@ -157,9 +172,9 @@ char	*string;
 	if (s != string) {
 		strcpy(bfr, string)[s-string] = EOS;
 		exts->if_ext = txtalloc(bfr);
-		VERBOSE "if(%s) ", exts->if_ext);
+		if (show) TELL "if(%s) ", exts->if_ext);
 	}
-	VERBOSE "extension(%s)\n", exts->no_ext);
+	if (show) TELL "extension(%s)\n", exts->no_ext);
 }
 
 /*
@@ -177,11 +192,13 @@ char	*name;
 	struct	stat	sb;
 
 	for (p = exts; p; p = p->link) {
-		if ((len = strlen(p->no_ext)) <= off) {
-			if (!strcmp(p->no_ext, &name[off-len])) {
+		if (p->no_ext == 0) {	/* "-i" test? */
+			if (!strwcmp(p->if_ext, name))
+				return (TRUE);
+		} else for (len = 0; len < off; len++) {
+			if (!strwcmp(p->no_ext, name + len)) {
 				if (p->if_ext) {	/* look for file */
-					(void)strcpy(strcpy(bfr, name)
-							+ off-len,
+					(void)strcpy(strcpy(bfr, name) + len,
 						p->if_ext);
 					if (stat(bfr, &sb) >= 0
 					&& (sb.st_mode & S_IFMT) == S_IFREG)
@@ -210,17 +227,33 @@ indent(level)
 }
 
 static
-pipes(path, name)
-char	*path, *name;
+pipes(path, name, vers)
+char	*path, *name, *vers;
 {
 	auto	char	tmp[BUFSIZ];
 
 	if ((verbose < 0) || !isatty(fileno(stdout))) {
+		if (config_rev) {
+			if (*vers != '?')
+				PRINTF("%s ", vers);
+			else
+				return;
+		}
 		path = pathcat(tmp, path, name);
 		if (original != 0)
 			path = relpath(path, original, path);
 		PRINTF("%s\n", path);
 	}
+}
+
+static
+char	*
+compared(what, rev)
+char	*what, *rev;
+{
+	static	char	buffer[80];
+	FORMAT(buffer, "%s than %s", what, rev);
+	return (buffer);
 }
 
 /*
@@ -237,9 +270,11 @@ struct	stat	*sp;
 {
 	char	*change	= 0,
 		*vers,
-		*owner;
+		*owner,
+		*locked_by = 0;
 	time_t	cdate;
 	int	mode	= (sp != 0) ? (sp->st_mode & S_IFMT) : 0;
+	int	ok_text;
 
 	if (mode == S_IFDIR) {
 		auto	char	tmp[BUFSIZ],
@@ -273,7 +308,13 @@ struct	stat	*sp;
 		rcslast(path, name, &vers, &cdate, &owner);
 		if ((cdate == 0) && (*vers == '?'))
 			sccslast(path, name, &vers, &cdate, &owner);
+		if (*owner != EOS && *owner != '?')
+			locked_by = owner;
+		else
+			owner = "";
+
 		if (cdate != 0) {
+			ok_text = TRUE;	/* assume this anyway */
 			if (cdate == sp->st_mtime) {
 				if (obsolete)
 					;	/* interpret as obsolete-rev */
@@ -285,26 +326,27 @@ struct	stat	*sp;
 						change = vers;
 				}
 			} else if (cdate > sp->st_mtime) {
-				change	= "older";
+				change	= compared("older", vers);
 			} else if (cdate < sp->st_mtime) {
-				change	= "newer";
+				change	= compared("newer", vers);
 			}
-		} else if (istextfile(name)) {
+		} else if (ok_text = istextfile(name)) {
 			change	= "not archived";
 		}
-		if ((change != 0) || (*owner != EOS && *owner != '?')) {
+		if ((change != 0) || locked_by != 0) {
 			if (change == 0)	change	= "no change";
-			if (*owner == '?')	owner	= "";
 			indent(level);
 			TELL "%s (%s%s%s)\n",
 				name,
 				change,
-				*owner ? ", locked by " : "",
+				(locked_by != 0) ? ", locked by " : "",
 				owner);
-			pipes(path, name);
+			pipes(path, name, vers);
 		} else if (debug) {
 			indent(level);
-			TELL "%s (ok)\n", name);
+			TELL "%s (%s)\n", name, ok_text ? vers : "binary");
+			if (ok_text && config_rev)
+				pipes(path, name, vers);
 		}
 	}
 	return(readable);
@@ -368,12 +410,10 @@ int	level;
 				if (show) {
 					indent(level);
 					TELL "%s (obsolete:%s)\n", tname, vers);
-					pipes(path, name);
+					pipes(path, name, vers);
 				} else if (debug) {
 					indent(level);
-					TELL "%s (ok) %s\n",
-						tname,
-						(cdate == 0) ? vers : "");
+					TELL "%s (%s)\n", tname, vers);
 				}
 			}
 		}
@@ -396,20 +436,40 @@ char	*name;
 usage()
 {
 	static	char	*msg[] = {
-	"usage: checkup [options] [directory [...]]",
-	"",
-	"options:",
-	"  -a      report files and directories beginning with \".\"",
-	"  -d      debug (show all names)",
-	"  -l FILE write logfile of tree-of-files",
-	"  -o      reports obsolete files (no working file exists)",
-	"  -p      generate pathnames relative to current directory",
-	"  -q      quiet",
-	"  -r REV  specifies revision-level to check against, REV+ to reverse",
-	"  -s      (same as -q)",
-	"  -t      suppresses standard extensions: .bak .i .log .out .tmp",
-	"  -v      verbose",
-	"  -x EXT  extension rules: .EXT to suppress, .IF.THEN conditionally"
+"Usage: checkup [options] [directory [...]]",
+"",
+"A report is written to standard-error showing the directory trees which are",
+"scanned, and the check-in status of each source-file.  If standard-output is",
+"not a terminal (i.e., redirected to a file) it receives a list of the file",
+"names reported.",
+"",
+"Options:",
+"  -a      report files and directories beginning with \".\"",
+"  -c      display configuration-revisions of each file.  This causes the list",
+"          of files written to standard-output to include only checked-in",
+"          names, with their revision codes.",
+"  -d      debug (show all names)",
+"  -i TEXT suppress leaves matching \"TEXT\"",
+"  -l FILE write logfile of tree-of-files",
+"  -o      reports obsolete files (no working file exists)",
+"  -p      generate pathnames relative to current directory",
+"  -q      quiet: suppresses directory-tree report",
+"  -r REV  specifies revision-level to check against, reporting all working",
+"          files whose highest checked-in version is below REV.",
+"          Use REV+ to reverse, showing files above REV.",
+"  -s      (same as -q)",
+"  -t      suppresses standard extensions: .bak .i .log .out .tmp",
+"  -v      verbose: prints names (to stderr) which cannot be processed (e.g.,",
+"          sockets and special devices).",
+"  -x TEXT specifies an extension to be ignored.  The first character in the",
+"          option-value is the delimiter.  If repeated, it marks off a second",
+"          extension which must correspond to an existing file before the",
+"          matching file is ignored. The cases are:",
+"          .EXT to suppress unconditionally,",
+"          .IF.THEN conditionally",
+"",
+"The -i \"TEXT\" and -x \".EXT\" and \".THEN\" strings permit the use of the",
+"wildcards \"*\" and \"?\".  The -i and -x options are processed in LIFO order."
 	};
 	register int	j;
 	for (j = 0; j < sizeof(msg)/sizeof(msg[0]); j++)
@@ -437,13 +497,19 @@ char	*argv[];
 	(void)setlinebuf(stderr);
 #endif
 
-	while ((j = getopt(argc, argv, "adl:opqr:stvx:")) != EOF)
+	while ((j = getopt(argc, argv, "acdi:l:opqr:stvx:")) != EOF)
 		switch (j) {
 		case 'a':
 			allnames++;
 			break;
+		case 'c':
+			config_rev++;
+			break;
 		case 'd':
 			debug++;
+			break;
+		case 'i':
+			ignore(optarg);
 			break;
 		case 'l':
 			if (!fopen(optarg, "a+"))
