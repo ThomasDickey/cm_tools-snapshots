9ad6e6654c4705f0a02a36c12da8458e9e073c23
Thomas E. Dickey <dickey@invisible-island.net>
1991-07-15 12:59:07 -0400

    snapshot of project "cm_tools", label t19910715

--- src/checkout/src/checkout.c
+++ src/checkout/src/checkout.c
@@ -1,83 +1,31 @@
 #ifndef	lint
-static	char	Id[] = "$Header: /users/source/archives/cm_tools.vcs/src/checkout/src/RCS/checkout.c,v 9.1 1991/06/20 11:37:36 dickey Exp $";
+static	char	Id[] = "$Header: /users/source/archives/cm_tools.vcs/src/checkout/src/RCS/checkout.c,v 9.2 1991/07/11 14:47:28 dickey Exp $";
 #endif
 
 /*
  * Title:	checkout.c (front end for RCS checkout)
  * Author:	T.E.Dickey
  * Created:	20 May 1988 (from 'sccsdate.c')
- * $Log: checkout.c,v $
- * Revision 9.1  1991/06/20 11:37:36  dickey
- * use 'shoarg()'
- *
- *		Revision 9.0  91/06/11  08:25:19  ste_cm
- *		BASELINE Tue Jun 11 08:26:48 1991 -- apollo sr10.3
- *		
- *		Revision 8.2  91/06/11  08:25:19  dickey
- *		lint (apollo sr10.2)
- *		
- *		Revision 8.1  91/05/20  12:38:55  dickey
- *		mods to compile on apollo sr10.3
- *		
- *		Revision 8.0  90/08/14  14:02:29  ste_cm
- *		BASELINE Tue Aug 14 14:11:43 1990 -- ADA_TRANS, LINCNT
- *		
- *		Revision 7.1  90/08/14  14:02:29  dickey
- *		lint
- *		
- *		Revision 7.0  90/04/19  08:03:33  ste_cm
- *		BASELINE Mon Apr 30 09:54:01 1990 -- (CPROTO)
- *		
- *		Revision 6.2  90/04/19  08:03:33  dickey
- *		corrected so we don't pass "-x" to 'co'
- *		
- *		Revision 6.1  90/04/19  07:39:36  dickey
- *		added "-x" option (to help with makefiles, etc)
- *		
- *		Revision 6.0  89/12/12  10:31:39  ste_cm
- *		BASELINE Thu Mar 29 07:37:55 1990 -- maintenance release (SYNTHESIS)
- *		
- *		Revision 5.1  89/12/12  10:31:39  dickey
- *		lint (SunOs 4.0.3)
- *		
- *		Revision 5.0  89/10/10  16:18:15  ste_cm
- *		BASELINE Fri Oct 27 12:27:25 1989 -- apollo SR10.1 mods + ADA_PITS 4.0
- *		
- *		Revision 4.3  89/10/10  16:18:15  dickey
- *		corrected last change (if no directory name is explicit,
- *		use ".")
- *		
- *		Revision 4.2  89/08/24  16:11:44  dickey
- *		exit with error if 'usercopy()' fails
- *		
- *		Revision 4.1  89/08/24  15:32:37  dickey
- *		suppress 'revert()' message if user does not want to lock the
- *		file.  also, verify that working file's directory exists
- *		(otherwise 'usercopy()' silently fails...)
- *		
- *		Revision 4.0  89/08/22  09:54:46  ste_cm
- *		BASELINE Thu Aug 24 09:27:38 EDT 1989 -- support:navi_011(rel2)
- *		
- *		Revision 3.1  89/08/22  09:54:46  dickey
- *		corrected format of debug-message
- *		
- *		Revision 3.0  89/03/29  14:48:40  ste_cm
- *		BASELINE Mon Jun 19 13:12:40 EDT 1989
- *		
- *		Revision 2.0  89/03/29  14:48:40  ste_cm
- *		BASELINE Thu Apr  6 09:22:38 EDT 1989
- *		
- *		Revision 1.31  89/03/29  14:48:40  dickey
- *		if we cannot find the archive, this may be because the user has
- *		hidden his directory (and therefore we cannot see it in setuid
- *		mode).  revert to normal rights in this case and try again.
- *		
- *		Revision 1.30  89/03/28  15:08:13  dickey
- *		forgot to invoke 'rcspermit()' to check directory-level permit.
- *		
- *		Revision 1.29  89/03/21  14:12:59  dickey
- *		sccs2rcs keywords
- *		
+ * Modified:
+ *		11 Jul 1991, make this work properly with suid-root
+ *		20 Jun 1990, use 'shoarg()'
+ *		20 May 1991, mods to compile on apollo sr10.3
+ *		19 Apr 1990, corrected so we don't pass "-x" to 'co'.
+ *			     added "-x" option (to help with makefiles, etc)
+ *		10 Oct 1989, corrected last change (if no directory name is
+ *			     explicit, use ".")
+ *		24 Aug 1989, exit with error if 'usercopy()' fails.  Suppress
+ *			     'revert()' message if user does not want to lock
+ *			     the file.  Also, verify that working file's
+ *			     directory exists (otherwise 'usercopy()' silently
+ *			     fails...)
+ *		22 Aug 1989, corrected format of debug-message
+ *		29 Mar 1989, if we cannot find the archive, this may be because
+ *			     the user has hidden his directory (and therefore
+ *			     we cannot see it in setuid mode).  Revert to normal
+ *			     rights in this case and try again.
+ *		28 Mar 1989, forgot to invoke 'rcspermit()' to check directory-
+ *			     level permit.
  *		21 Mar 1989, strip setuid privilege if caller wants to check out
  *			     a file not owned by the setuid process.
  *		24 Jan 1989, don't insist that archive be owned by user or euid
@@ -353,7 +301,7 @@ int	owner;			/* TRUE if euid, FALSE if uid */
 					(void)strcpy(RCSdir, "./");
 					DEBUG(("++ RCSdir='%s'\n", RCSdir))
 
-				if (Effect != sb.st_uid) {
+				if (Effect != sb.st_uid && Effect != 0) {
 					revert(debug ? "non-CM use":(char *)0);
 					Effect = geteuid();
 				} else if (!rcspermit(RCSdir,(char *)0))
@@ -385,7 +333,7 @@ permit(name, sb_, uid)
 char		*name;
 struct	stat	*sb_;
 {
-	if (uid != sb_->st_uid) {
+	if (uid != sb_->st_uid && Effect != 0) {
 		DEBUG(("=> uid  = %d, file = %d\n", uid, sb_->st_uid))
 		noPERM(name);
 	}
